4140631a1cc3050c909062f2d2cbeb74d5522524
eanorambuena <eanorambuena@uc.cl>
2021-07-14 01:08:07 -0400

    Improve install and add upgrade function

--- egg/resources/modules.py
+++ egg/resources/modules.py
@@ -1,10 +1,41 @@
-import pip
+import pip, sys, subprocess
 import importlib as il
 from egg.resources.constants import white
 
 def install(name: str):
+    try:
+        install_option_1(name)
+        raise Exception('error')
+    except:
+        print("Install failed")
+        print("Retrying...")
+        try:
+            install_option_2(name)
+        except:
+            print("Install failed")
+            return "error"
+    print(name+" succesfully installed")
+    return "done"
+        
+def install_option_1(name: str):
+    #Implement pip using pip package
     pip.main(['install', name])
-    return "Done"
+    return "done"
+
+def install_option_2(name: str):
+    #Implement pip as a subprocess
+    if name=="$upgrade":
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+    else:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', name])
+    return "done"
+
+def upgrade(name: str):
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', name])
+    except:
+        print(name+" succesfully ugraded")
+    return "done"
 
 class Repo():
     def __init__(self,name: str):
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-#from nqs import write, run
-from egg import sleep, eggConsole, install, Repo
+from egg import *
 
-f="example2"
 #eggConsole()
-#run(f)
-#write(f)
 
-install("matplotlib")
-
+#install("$upgrade")
+#upgrade("matplotlib")
+"""
 covid=Repo("CovidPlot")
 cp=covid.pull("covidplot","CovidData")
 #pull("CovidPlot")
 covidata=cp.CovidData
 c=covidata()
 c.plot
+"""
